5b2cfeb66d503d6543f650a62068d474f676e697
kiwijuice56 <alfaroeric127@gmail.com>
2024-07-22 18:11:46 -0500

    Fixed requests library import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 from flask import Flask, request
 
@@ -12,7 +13,7 @@ def get_url():
     request_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId={playlist_id}&key={os.getenv("YOUTUBE_API_KEY")}"
     if "page_token" in request.args:
         request_url += f"&pageToken={request.args.get("page_token")}"
-    return request.get(request_url).content
+    return requests.get(request_url).content
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
